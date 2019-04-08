d3f48fad4fceca8c422bff0e5ad87c245b911a3a
Lucas Lauriano de Souza <lucaslauriano@live.com>
2019-04-07 23:54:34 -0300

    Updating Environment

--- src/commons/environment.js
+++ src/commons/environment.js
@@ -1,6 +1,8 @@
+require('custom-env').env();
+
 module.exports = {
-  server: 'http://localhost:3000/api/users', //process.env.DEV_SERVER,
-  github_api: 'https://api.github.com/users', //process.env.GITHUB_API_URL,
-  github_user_id: 'lucaslauriano', //process.env.GITHUB_USER_ID,
-  github_user_key: 'github.456852' //process.env.GITHUB_USER_KEY
+  server: process.env.DEV_SERVER,
+  github_api: process.env.GITHUB_API_URL,
+  github_user_id: process.env.GITHUB_USER_ID,
+  github_user_key: process.env.GITHUB_USER_KEY
 };
