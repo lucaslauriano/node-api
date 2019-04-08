eaf3729a850018119501d64a4b6d4b4d3234810b
Lucas Lauriano de Souza <lucaslauriano@live.com>
2019-04-08 01:26:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const requireDir = require('require-dir');
+
 require('dotenv').config();
 
+const PORT = process.env.PORT || 5000;
 //Iniciando o App
 const app = express();
 app.use(express.json());
@@ -13,4 +15,4 @@ requireDir('./src/models');
 // Routes
 app.use('/api', require('./src/routes'));
 
-app.listen(8080);
+app.listen(PORT);
